bdcf6853186554cd155becfa1908a6aa22ab90ca
Edwin Wen <edwinwen75@gmail.com>
2024-10-29 17:45:10 -0400

    meeting 3

--- player.gd
+++ player.gd
@@ -7,6 +7,15 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(delta):
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.play("Idle")
+	else:
+		$AnimatedSprite2D.play("Walk")
+		if Input.is_action_just_pressed("right"):
+			$AnimatedSprite2D.flip_h = false
+		elif Input.is_action_just_pressed("left"):
+			$AnimatedSprite2D.flip_h = true
 
 func _physics_process(delta):
 	# Add the gravity.
