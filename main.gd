4ce902d02c597cf3051e58f5608326e16bf3f36b
Edwin Wen <edwinwen75@gmail.com>
2024-11-26 15:50:52 -0500

    Nov. 26 meeting

--- main.gd
+++ main.gd
@@ -2,9 +2,14 @@ extends Node2D
 var levels = 3
 var curr_level = 1
 
+var player_location = [Vector2(188, 533), Vector2(74, 119)]
+var door_position = [Vector2(1111, 395), Vector2(978, 327)]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Player.position = player_location[curr_level-1]
+	$Door.position = door_position[curr_level-1]
 	$Levels.show_level(curr_level, levels)
 
 
@@ -15,4 +20,6 @@ func _process(delta):
 
 func _on_door_advance():
 	curr_level += 1
+	$Player.position = player_location[curr_level-1]
+	$Door.position = door_position[curr_level-1]
 	$Levels.show_level(curr_level, levels)
